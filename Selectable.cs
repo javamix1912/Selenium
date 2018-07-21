f4437ee8abf371c0e51e8d5b1ea40e9ab6a3f27f
idafov <javamix1912@gmail.com>
2018-07-21 17:06:03 +0300

    Select Item Positive test

---
+++ Selectable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace DemoQA_Registration
+{
+    public class Selectable
+    {
+        [Test]
+        public void Select()
+        {
+            IWebDriver _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Url = "http://demoqa.com/selectable/";
+
+            IWebElement item7 = _driver.FindElement(By.Id("selectable"));
+            IWebElement item77 = item7.FindElement(By.XPath(@"//*[@id=""selectable""]/li[7]"));
+            string getClassItem7Before = item77.GetAttribute("class");
+            item77.Click();
+
+
+            string getClassItem7After = item7.GetAttribute("class");
+            Assert.AreNotEqual(getClassItem7Before, getClassItem7After);
+        }
+    }
+}
