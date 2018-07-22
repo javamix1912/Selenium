dbd6f90ee416fddf7c8c66537990bb8ab9d3c1c3
idafov <javamix1912@gmail.com>
2018-07-22 15:27:14 +0300

    Short Test that assert the page is open

---
+++ Selenium-Fund-HW.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Selenium_Fundamentals_HW
+{
+    class Selenium_Fund_HW
+    {
+        IWebDriver _driver;
+        [Test]
+        public void GoogleSearchSelenium()
+        {
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Manage().Window.Maximize();
+            _driver.Url = "http://www.google.com";
+
+            IWebElement searchBox = _driver.FindElement(By.Id("lst-ib"));
+            searchBox.Clear();
+            searchBox.SendKeys("Selenium");
+            searchBox.SendKeys(Keys.Enter);
+
+            
+            IWebElement clickHere = _driver.FindElement(By.LinkText("Selenium - Web Browser Automation"));
+          
+            clickHere.Click();
+
+            Assert.AreEqual("Selenium - Web Browser Automation", _driver.Title);
+
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
