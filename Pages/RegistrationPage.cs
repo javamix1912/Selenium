4f8da4cfa6767f38888930fe948aa1aebfae928a
idafov <javamix1912@gmail.com>
2018-07-30 18:12:10 +0300

    DemoQA-WebPage-Auto-Registration-v2
    PageObject Mode

---
+++ Pages/RegistrationPage.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoQA_Registration.Pages
+{
+    public partial class RegistrationPage
+    {
+        private IWebDriver _driver;
+        public RegistrationPage(IWebDriver driver)
+        {
+            this._driver = driver;
+        }
+        public string Url { get; set; }
+
+
+        public void FillRegistrationForm()
+        {
+            Type(FirstName, "IvanIvanIvan");
+            Type(LastName, "Family Ivan");
+            Status[0].Click();
+            Hobby[2].Click();
+            Country.SelectByText("Bulgaria");
+            Month.SelectByValue("12");
+            Day.SelectByValue("25");
+            Year.SelectByValue("1997");
+            //Random number generator
+            var charsNum = "1234567890";
+            var numChars = new char[10];
+            var randomNum = new Random();
+
+            for (int i = 0; i < numChars.Length; i++)
+            {
+                numChars[i] = charsNum[randomNum.Next(charsNum.Length)];
+            }
+
+            var finalNumString = new String(numChars);
+            Type(Phone, finalNumString);
+            //End random number Generator
+
+            //RANDOM USERNAME
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[10];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            Type(UserName, finalString);
+            //End random username Generator
+
+
+
+            //RANDOM EMAIL
+            var chars2 = "ABCDEFGHIJKLMNabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars2 = new char[10];
+            var randomEmail = new Random();
+
+            for (int i = 0; i < stringChars2.Length; i++)
+            {
+                stringChars2[i] = chars2[randomEmail.Next(chars2.Length)];
+            }
+
+            var finalStringEmail = new String(stringChars2);
+            Type(Email, finalStringEmail + "@gmail.com");
+            //End random email Generator
+
+            Type(Description, "This is Written By Selenium Webdriver and I am LHMDAI !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+            //RANDOM PASSWORD
+            var chars3 = "ABCDLMNZXCabcdopqtuvwxyz0123456789!@#$%^&*";
+            var stringChars3 = new char[16];
+            var randomPass = new Random();
+
+            for (int i = 0; i < stringChars3.Length; i++)
+            {
+                stringChars3[i] = chars3[randomPass.Next(chars3.Length)];
+            }
+
+            var finalStringPass = new String(stringChars3);
+            Type(Password, finalStringPass);
+            Type(PasswordConfirm, finalStringPass);
+
+            WebDriverWait wait2 = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
+            wait2.Until(ExpectedConditions.ElementToBeClickable(RegisterMeButton));
+
+            Assert.AreEqual("Strong", PasswordMeter.Text);
+            RegisterMeButton.Click();
+
+            Assert.AreEqual("Thank you for your registration", IsRegistrationSuccessfull.Text);
+        }
+
+        private void Type(IWebElement element, string text)
+        {
+            element.Clear();
+            element.SendKeys(text);
+        }
+    }   
+}
---
+++ Pages/RegistrationPageMap.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoQA_Registration;
+using Register_DemoQA;
+using OpenQA.Selenium.Support.UI;
+
+namespace DemoQA_Registration.Pages
+{
+    public partial class RegistrationPage
+    {
+        public IWebElement FirstName => _driver.FindElement(By.Id("name_3_firstname"));
+        public IWebElement LastName => _driver.FindElement(By.Id("name_3_lastname"));
+        public List<IWebElement> Status => _driver.FindElements(By.Name("radio_4[]")).ToList();
+        public List<IWebElement> Hobby => _driver.FindElements(By.Name("checkbox_5[]")).ToList();
+
+        public SelectElement Country => new SelectElement(_driver.FindElement(By.Id("dropdown_7")));
+        public SelectElement Month => new SelectElement(_driver.FindElement(By.Id("mm_date_8")));
+        public SelectElement Day => new SelectElement(_driver.FindElement(By.Id("dd_date_8")));
+        public SelectElement Year => new SelectElement(_driver.FindElement(By.Id("yy_date_8")));
+
+        public IWebElement Phone => _driver.FindElement(By.Id("phone_9"));
+        public IWebElement UserName => _driver.FindElement(By.Id("username"));
+        public IWebElement Email => _driver.FindElement(By.Id("email_1"));
+        public IWebElement Description => _driver.FindElement(By.Id("description"));
+
+        public IWebElement Password => _driver.FindElement(By.Id("password_2"));
+
+        public IWebElement PasswordConfirm => _driver.FindElement(By.Id("confirm_password_password_2"));
+
+        public IWebElement PasswordMeter => _driver.FindElement(By.Id("password_meter"));
+
+        public IWebElement RegisterMeButton => _driver.FindElement(By.Name("pie_submit"));
+
+        public IWebElement IsRegistrationSuccessfull => _driver.FindElement(By.ClassName("piereg_message"));
+    }
+}
